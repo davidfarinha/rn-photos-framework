6644ed953476711b7086f9491262a94672f3e620
David Farinha <dfarinha@mobiquityinc.com>
2020-09-27 20:24:03 +0100

    Hotfix

--- ios/RNPhotosFramework/PHAssetsService.m
+++ ios/RNPhotosFramework/PHAssetsService.m
@@ -152,6 +152,7 @@
         [arrayWithResourcesMetadata addObject:@{
                                                      @"originalFilename" : resourceMetadata.originalFilename,
                                                      @"assetLocalIdentifier" : resourceMetadata.assetLocalIdentifier,
+													 @"fileSize": [asset valueForKey:@"fileSize"],
                                                      @"uniformTypeIdentifier" : resourceMetadata.uniformTypeIdentifier,
                                                      @"type" : [[RCTConvert PHAssetResourceTypeValuesReversed] objectForKey:@(resourceMetadata.type)],
                                                      @"mimeType" : mimeType,
